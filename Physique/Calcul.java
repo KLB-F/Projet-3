f856d7e2cec0619da8b7891f4c2a960c59764551
KLB-F <killiann.lebellec@gmail.com>
2025-06-23 15:19:17 +0200

    Add files via upload

--- Physique/Calcul.java
+++ Physique/Calcul.java
@@ -10,20 +10,26 @@ public class Calcul {
 		return (EcVoulue-EcAc)/dt; //Le nom change, mais la méthode reste la mêm
 	}
 	
-	static public float[] cal_CinInverse(float dt, float DX, float DY, float posXAct, float posYAct,float angleArt1, float angleArt2, float angleArt3, float longueur1, float longueur2, float longueur3, float posX3) {
-		float d = (float) Math.sqrt((double)(DX*DX+DY*DY));
-		if(d == 0.f) { //Cas distance nul
-			return new float[]{angleArt1, angleArt2, angleArt3};
+	static public float[] cal_CinInverse(float dt, float angle1,float x, float y, float l1, float l2) {
+		float d = (float) Math.sqrt(x*x + y*y);
+	    float c2 = (x*x + y*y - l1*l1 - l2*l2) / (2 * l1 * l2);
+
+	    if (c2 < -1 || c2 > 1) {
+	        // Hors de portée
+	        return new float[]{angle1, 0, 0};
+	    }
+		
+		if(c2 < -1 || c2 > 1) {
+			return new float[] {angle1, 0, 0};
 		}
-		float Theta1;
-		if((double)((((longueur2+5)*(longueur2+5)+d*d-(longueur3+5)*(longueur3+5))/(2*(longueur2+5)*d))) > 1.d) { //Trop loins
-			return new float[]{angleArt1, angleArt2, angleArt3};
-		}
-		float ThetaT = (float)Math.acos((double)(DX)/d);
-		Theta1 = (float)Math.acos((double)((((longueur2+5)*(longueur2+5)+d*d-(longueur3+5)*(longueur3+5))/(2*(longueur2+5)*d))))+ThetaT;
-		float Theta2 = (float)(Math.PI - Math.acos(((longueur2+5)*(longueur2+5)+(longueur3+5)*(longueur3+5)-d*d)/(2*(longueur2+5)*(longueur3+5))));
-		System.out.print(((longueur2+5)*(longueur2+5)+d*d-(longueur3+5)*(longueur3+5))/(2*(longueur2+5)*d)); System.out.print(" "); System.out.println(d);
-		return new float[]{angleArt1, (Theta1), Theta2};
+		
+		float Theta2 = (float)Math.atan2(Math.sqrt(1-c2*c2), c2);
+		float Theta1 = (float)(Math.atan2(y, x)-Math.atan2(l2*Math.sqrt(1-c2*c2), l1+l2*c2));
+		
+		System.out.print("DX : "); System.out.print(x); System.out.print(" DY : "); System.out.println(y);
+		System.out.print(Theta1); System.out.print(" "); System.out.print(Theta2); System.out.println(" ");
+		
+		return new float[]{angle1, Theta1, Theta2};
 	}
 	
 }
--- Principale/Main.java
+++ Principale/Main.java
@@ -11,6 +11,9 @@ public class Main {
 		Physique physique = new Physique();
 		Fenetre fenetre = new Fenetre(physique.l_articulation, physique.pince, physique.batie);
 		
+		float posXV = physique.l_articulation[2].donnePositionAttache()[0];
+		float posYV = physique.l_articulation[2].donnePositionAttache()[1];
+		
 		while(true) {
 			if(fenetre.donnerMode() == "Angle") {
 				float[] AngVoulue = fenetre.donnerListeAngleVoulue();
@@ -21,28 +24,23 @@ public class Main {
 				physique.udp_physique((float)30/1000, v_ang, Calcul.cal_EcartementToVitEcartement((float)30/1000, fenetre.donnerEcPince(), physique.pince.getEcartement()));
 			}else if(fenetre.donnerMode() == "Direction"){
 				String dir = fenetre.donnerDir();
-				float posXA = physique.l_articulation[2].donnePositionAttache()[0];
-				float posYA = physique.l_articulation[2].donnePositionAttache()[1];
 				
-				float posXV = posXA; float posYV = posYA;
 				if(dir == "D") {
-					posXV = posXA+5.f;posYV = posYA;
+					posXV = posXV+0.5f;posYV = posYV;
 				}else if(dir == "G") {
-					posXV = posXA-5.f;posYV = posYA;
+					posXV = posXV-0.5f;posYV = posYV;
 				}else if(dir == "H") {
-					posXV = posXA;posYV = posYA+5.f;
-					System.out.println("H");
+					posXV = posXV;posYV = posYV-0.5f;
 				}else if(dir == "B") {
-					posXV = posXA;posYV = posYA-5.f;
+					posXV = posXV;posYV = posYV+0.5f;
 				}else{
-					posXV = posXA;posYV = posYA;
+					posXV = posXV;posYV = posYV;
 				}
 				System.out.print("X : "); System.out.print(physique.l_articulation[0].donnePositionAttache()[0]); System.out.print(" Y : "); System.out.println(physique.l_articulation[0].donnePositionAttache()[1]);
 				System.out.print("X2 : "); System.out.print(posXV); System.out.print(" Y : "); System.out.println(posYV);
-				System.out.print("DX : "); System.out.print(posXV-physique.l_articulation[0].donnePositionAttache()[0]); System.out.print("DY : "); System.out.println(posYV-physique.l_articulation[0].donnePositionAttache()[1]);
 				float angle1 = physique.l_articulation[0].donnerAngle();float angle2 = physique.l_articulation[1].donnerAngle();float angle3 = physique.l_articulation[2].donnerAngle();
 				float longu1 = physique.l_articulation[0].donnerLongueur(); float longu2 = physique.l_articulation[1].donnerLongueur();float longu3 = physique.l_articulation[2].donnerLongueur();
-				float Angl[] = Calcul.cal_CinInverse((float)(30/1000), posXV-physique.l_articulation[0].donnePositionAttache()[0], posYV-physique.l_articulation[0].donnePositionAttache()[1], physique.l_articulation[0].donnePositionAttache()[0], physique.l_articulation[0].donnePositionAttache()[1], angle1, angle2, angle3, longu1, longu2, longu3, physique.pince.donnePosition()[0]);
+				float Angl[] = Calcul.cal_CinInverse((float)(30/1000), angle1,posXV-physique.l_articulation[0].donnePositionAttache()[0], posYV-physique.l_articulation[0].donnePositionAttache()[1], longu2+5, longu3+5);
 				float[] v_ang = new float[physique.l_articulation.length];
 				for(int i = 0; i < physique.l_articulation.length; i++) {
 					v_ang[i] = Calcul.cal_AngleToVitAng((float)30/1000, Angl[i], physique.l_articulation[i].donnerAngle());
